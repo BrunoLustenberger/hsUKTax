af4b9e07f4933fdf40b4fbe0b33854162dc99a21
bumasi <brn.lstnbrgr@gmail.com>
2021-09-14 21:18:30 +0200

    test cases finished

--- UKIncomeTaxTests.hs
+++ UKIncomeTaxTests.hs
@@ -51,6 +51,7 @@ qc_pa2 = quickCheck (withMaxSuccess 1000  prop_pa2)
 qc_pa3 = quickCheck (withMaxSuccess 1000  prop_pa3)
 qc_pa4 = quickCheck (withMaxSuccess 1000  prop_pa4)
 
+
 {- 1.3 taxable income -}
 
 -- ==0 if income lowest
@@ -89,24 +90,26 @@ prop_it2 incTest =
 qc_it1 = quickCheck (withMaxSuccess 1000  prop_it1)
 qc_it2 = quickCheck (withMaxSuccess 1000  prop_it2)
 
+--pendenz: income == pa + summe er 3 chunks
+
 {- 2. Unit Tests with HUnit -}
 
 {- 2.1 personalAllowance -}
 
-pa1 = TestCase (assertEqual "pa1" (personalAllowance (-1))      12500)
-pa2 = TestCase (assertEqual "pa2" (personalAllowance 0)         12500)
-pa3 = TestCase (assertEqual "pa3" (personalAllowance 1)         12500)
-pa4 = TestCase (assertEqual "pa4" (personalAllowance 99999)     12500)
-pa5 = TestCase (assertEqual "pa5" (personalAllowance 100000)    12500)
-pa6 = TestCase (assertEqual "pa6" (personalAllowance 100001)    12500)
-pa7 = TestCase (assertEqual "pa7" (personalAllowance 100002)    12499)
-pa8 = TestCase (assertEqual "pa8" (personalAllowance 100003)    12499)
-pa8' = TestCase (assertEqual "pa8'" (personalAllowance 100004)  12498)
-pa9 = TestCase (assertEqual "pa9"   (personalAllowance 124997)  2)
-pa10 = TestCase (assertEqual "pa10" (personalAllowance 124998)  1)
-pa11 = TestCase (assertEqual "pa11" (personalAllowance 124999)  1)
-pa12 = TestCase (assertEqual "pa12" (personalAllowance 125000)  0)
-pa13 = TestCase (assertEqual "pa13" (personalAllowance 125001)  0)
+pa1  = TestCase $ assertEqual "pa1"     (personalAllowance (-1))        12500
+pa2  = TestCase $ assertEqual "pa2"     (personalAllowance 0)           12500
+pa3  = TestCase $ assertEqual "pa3"     (personalAllowance 1)           12500
+pa4  = TestCase $ assertEqual "pa4"     (personalAllowance 99999)       12500
+pa5  = TestCase $ assertEqual "pa5"     (personalAllowance 100000)      12500
+pa6  = TestCase $ assertEqual "pa6"     (personalAllowance 100001)      12500
+pa7  = TestCase $ assertEqual "pa7"     (personalAllowance 100002)      12499
+pa8  = TestCase $ assertEqual "pa8"     (personalAllowance 100003)      12499
+pa8' = TestCase $ assertEqual "pa8'"    (personalAllowance 100004)      12498
+pa9  = TestCase $ assertEqual "pa9"     (personalAllowance 124997)      2
+pa10 = TestCase $ assertEqual "pa10"    (personalAllowance 124998)      1
+pa11 = TestCase $ assertEqual "pa11"    (personalAllowance 124999)      1
+pa12 = TestCase $ assertEqual "pa12"    (personalAllowance 125000)      0
+pa13 = TestCase $ assertEqual "pa13"    (personalAllowance 125001)      0
 
 paTests = TestLabel "pa" (TestList [pa1,pa2,pa3,pa4,pa5,pa6,pa7,pa8,pa9,pa10,
                                     pa11,pa12,pa13])
@@ -148,19 +151,51 @@ it7   = TestCase $ assertEqual "it7"   (incomeTax 12502)     0 -- 0.4
 it8   = TestCase $ assertEqual "it8"   (incomeTax 12503)     0 -- 0.6
 it9   = TestCase $ assertEqual "it9"   (incomeTax 12504)     0 -- 0.8
 it10  = TestCase $ assertEqual "it10"  (incomeTax 12505)     1 -- 1.0
-{-
-it11  = TestCase $ assertEqual "it11"  (incomeTax 100003)   87504
-it12  = TestCase $ assertEqual "it12"  (incomeTax 100004)   87506
-it13  = TestCase $ assertEqual "it13"  (incomeTax 100005)   87507
-it14  = TestCase $ assertEqual "it14"  (incomeTax 124998)   124997
-it15  = TestCase $ assertEqual "it15"  (incomeTax 124999)   124998
-it16  = TestCase $ assertEqual "it16"  (incomeTax 125000)   125000
-it17  = TestCase $ assertEqual "it17"  (incomeTax 125001)   125001
--}
+
+it11  = TestCase $ assertEqual "it11"  (incomeTax 49998)     7499 -- 7499.60
+it12  = TestCase $ assertEqual "it12"  (incomeTax 49999)     7499 -- 7499.80
+it13  = TestCase $ assertEqual "it13"  (incomeTax 50000)     7500 -- 7500.00
+it14  = TestCase $ assertEqual "it14"  (incomeTax 50001)     7500 -- 7500.40
+it15  = TestCase $ assertEqual "it15"  (incomeTax 50002)     7500 -- 7500.80
+it16  = TestCase $ assertEqual "it16"  (incomeTax 50003)     7501 -- 7501.20
+
+it20  = TestCase $ assertEqual "it20"  (incomeTax 99998)     27499 -- 27499.20
+it21  = TestCase $ assertEqual "it21"  (incomeTax 99999)     27499 -- 27499.60
+it22  = TestCase $ assertEqual "it22"  (incomeTax 100000)    27500 -- 27500.00
+it23  = TestCase $ assertEqual "it23"  (incomeTax 100001)    27500 -- 27500.40
+it24  = TestCase $ assertEqual "it24"  (incomeTax 100002)    27501 -- 27501.20
+it25  = TestCase $ assertEqual "it25"  (incomeTax 100003)    27501 -- 27501.60
+it26  = TestCase $ assertEqual "it26"  (incomeTax 100004)    27502 -- 27502.40
+it27  = TestCase $ assertEqual "it27"  (incomeTax 100005)    27502 -- 27502.80
+it28  = TestCase $ assertEqual "it28"  (incomeTax 100006)    27503 -- 27503.60
+
+it30  = TestCase $ assertEqual "it30"  (incomeTax 124997)    42498 -- 42498.00
+it31  = TestCase $ assertEqual "it31"  (incomeTax 124998)    42498 -- 42498.80
+it32  = TestCase $ assertEqual "it32"  (incomeTax 124999)    42499 -- 42499.20
+it33  = TestCase $ assertEqual "it33"  (incomeTax 125000)    42500 -- 42500.00
+it34  = TestCase $ assertEqual "it34"  (incomeTax 125001)    42500 -- 42500.40
+it35  = TestCase $ assertEqual "it35"  (incomeTax 125002)    42500 -- 42500.80
+it36  = TestCase $ assertEqual "it36"  (incomeTax 125003)    42501 -- 42501.20
+
+it40  = TestCase $ assertEqual "it40"  (incomeTax 149997)    52498 -- 52498.80
+it41  = TestCase $ assertEqual "it41"  (incomeTax 149998)    52499 -- 52499.20
+it42  = TestCase $ assertEqual "it42"  (incomeTax 149999)    52499 -- 52499.60
+it43  = TestCase $ assertEqual "it43"  (incomeTax 150000)    52500 -- 52500.00
+it44  = TestCase $ assertEqual "it44"  (incomeTax 150001)    52500 -- 52500.45
+it45  = TestCase $ assertEqual "it45"  (incomeTax 150002)    52500 -- 52500.90
+it46  = TestCase $ assertEqual "it46"  (incomeTax 150003)    52501 -- 52501.35
+
+it50  = TestCase $ assertEqual "it50"  (incomeTax 150010)    52504 -- 52504.50
+it51  = TestCase $ assertEqual "it51"  (incomeTax 160000)    57000 -- 57000.00
+
 
 itTests = TestLabel "it" (TestList [it0, 
-                                    it1,it2,it3,it4,it5,it6,it7,it8,it9,it10{-,
-                                    it11,it12,it13,it14,it15,it16,it17-}])
+                                    it1,it2,it3,it4,it5,it6,it7,it8,it9,it10,
+                                    it11,it12,it13,it14,it15,it16,
+                                    it20,it21,it22,it23,it24,it25,it26,it27,it28,
+                                    it30,it31,it32,it33,it34,it35,it36,
+                                    it40,it41,it42,it43,it44,it45,it46,
+                                    it50,it51])
 
 
 
